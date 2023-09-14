88e5e1ab38fe55114f3659cbde3c26017d4a13b5
Fajarwz <fajarwindhuzulfikar@gmail.com>
2023-09-14 09:14:47 +0700

    done

---
+++ README.md
@@ -0,0 +1,26 @@
+# Unlocking Real-Time With WebSockets in Laravel with Soketi
+This is an implementation of WebSockets in Laravel with [Soketi](https://soketi.app/). A blog about this can be found here: [Unlocking Real-Time With WebSockets in Laravel with Soketi | Fajarwz](https://fajarwz.com/blog/unlocking-real-time-with-websockets-in-laravel-with-soketi/).
+
+## Installation
+
+### Composer Packages 
+```
+composer install
+```
+
+## Configuration
+
+### Create `.env` file from `.env.example`
+```
+cp .env.example .env
+```
+
+### Generate Laravel App Key
+```
+php artisan key:generate
+```
+
+## Run the App
+```
+php artisan serve
+```
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Events\NewEvent;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/event', function () {
+    NewEvent::dispatch(request()->msg);
+
+    return 'Message sent!';
+});
