5df1969d568087aff0cab81b651910040486ea62
Fajarwz <fajarwindhuzulfikar@gmail.com>
2023-09-23 22:32:11 +0700

    move chats and rooms route to auth group

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('chats', ChatController::class)->only(['store']);
+    Route::resource('rooms', RoomController::class)->only(['index', 'show']);
 });
 
 require __DIR__.'/auth.php';
-
-Route::resource('chats', ChatController::class)->only(['store']);
-Route::resource('rooms', RoomController::class)->only(['index', 'show']);
